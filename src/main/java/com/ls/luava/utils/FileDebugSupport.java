77f06326bac19a80e1619533a0254e8a6eb6a64c
yangzj <yangzj@leofs.com.cn>
2023-04-10 13:36:44 +0800

    更新调试配置支持统配符

--- src/main/java/com/ls/luava/utils/FileDebugSupport.java
+++ src/main/java/com/ls/luava/utils/FileDebugSupport.java
@@ -4,7 +4,6 @@ import com.google.common.base.Splitter;
 import com.google.common.base.Strings;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
-import com.ls.luava.spi.DebugSupport;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,30 +11,19 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.util.*;
 
-public class FileDebugSupport implements DebugSupport {
+public class FileDebugSupport extends BaseDebugSupport {
   static final Logger LOG = LoggerFactory.getLogger(DebugHelper.class);
   public static final Splitter SPLITTER = Splitter.on(",").omitEmptyStrings().trimResults();
 
   private final File file;
   private volatile long lastModified;
-  private final Map<String, Set<String>> map = Maps.newConcurrentMap();
 
   public FileDebugSupport(File file) {
     this.file = file;
   }
 
-  public boolean isDebug(String name){
-    reload();
-    Boolean value = map.containsKey(name);
-    return value;
-  }
-  public boolean isDebug(String name,String value){
-    reload();
-    Set<String> values = map.get(name);
-    return values!=null&&values.contains(value);
-  }
-
-  private synchronized void reload() {
+  @Override
+  protected synchronized void reload() {
     if(file.exists()&&file.lastModified()!=lastModified){
       lastModified = file.lastModified();
 
@@ -45,13 +33,15 @@ public class FileDebugSupport implements DebugSupport {
       } catch (Exception e) {
         LOG.warn("",e);
       }
-      this.map.clear();
+      this.clear();
       for (String key : properties.stringPropertyNames()) {
         String val = properties.getProperty(key);
         List<String> values = SPLITTER.splitToList(Strings.nullToEmpty(val));
-        map.put(key, Sets.newHashSet(values));
+        this.addValue(key, values);
       }
     }
   }
+
+
 
 }
--- src/main/java/com/ls/luava/utils/NoneDebugSupport.java
+++ src/main/java/com/ls/luava/utils/NoneDebugSupport.java
@@ -1,15 +1,8 @@
 package com.ls.luava.utils;
 
-import com.ls.luava.spi.DebugSupport;
 
-public class NoneDebugSupport implements DebugSupport {
-  @Override
-  public boolean isDebug(String name) {
-    return false;
-  }
+import java.util.Set;
 
-  @Override
-  public boolean isDebug(String name, String value) {
-    return false;
-  }
+public class NoneDebugSupport extends BaseDebugSupport {
+
 }
