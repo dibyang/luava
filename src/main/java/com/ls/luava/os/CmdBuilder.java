e059ddb4677883db47c1728bab58c2b7ac79fea6
yangzj <yangzj@leofs.com.cn>
2023-05-16 14:50:19 +0800

    添加实现Callable<CmdResult>接口

--- src/main/java/com/ls/luava/os/CmdBuilder.java
+++ src/main/java/com/ls/luava/os/CmdBuilder.java
@@ -3,134 +3,146 @@ package com.ls.luava.os;
 import java.io.IOException;
 import java.lang.reflect.Array;
 import java.util.*;
+import java.util.concurrent.Callable;
 
-public class CmdBuilder {
-    final List<String> cmds = new ArrayList<>();
-    final Map<String, String> environment = new HashMap<>();
+public class CmdBuilder implements Callable<CmdResult> {
+  final List<String> cmds = new ArrayList<>();
+  final Map<String, String> environment = new HashMap<>();
 
-    boolean shell = false;
-    boolean readOut = true;
-    boolean readErr = true;
-    boolean en = true;
-    boolean debug;
+  boolean shell = false;
+  boolean readOut = true;
+  boolean readErr = true;
+  boolean en = true;
+  boolean debug;
+  PreExecute preExecute;
 
-    protected CmdBuilder(String cmd) {
-        cmds.add(cmd);
-    }
+  protected CmdBuilder(String cmd) {
+    cmds.add(cmd);
+  }
 
-    public CmdBuilder en(boolean en) {
-        this.en = en;
-        return this;
-    }
+  public CmdBuilder en(boolean en) {
+    this.en = en;
+    return this;
+  }
 
-    public CmdBuilder debug(boolean debug) {
-        this.debug = debug;
-        return this;
-    }
+  public CmdBuilder debug(boolean debug) {
+    this.debug = debug;
+    return this;
+  }
 
-    public Map<String, String> getEnvironment() {
-        return environment;
-    }
 
-    public CmdBuilder setShell(boolean shell) {
-        this.shell = shell;
-        return this;
-    }
+  public CmdBuilder setPreExecute(PreExecute preExecute) {
+    this.preExecute = preExecute;
+    return this;
+  }
 
-	@Deprecated
-    public CmdBuilder setReadOut(boolean readOut) {
-        this.readOut = readOut;
-        return this;
-    }
+  public Map<String, String> getEnvironment() {
+    return environment;
+  }
 
-	@Deprecated
-    public boolean isReadOut() {
-        return readOut;
-    }
+  public CmdBuilder setShell(boolean shell) {
+    this.shell = shell;
+    return this;
+  }
 
-	@Deprecated
-    public CmdBuilder setReadErr(boolean readErr) {
-        this.readErr = readErr;
-        return this;
-    }
+  @Deprecated
+  public CmdBuilder setReadOut(boolean readOut) {
+    this.readOut = readOut;
+    return this;
+  }
 
-	@Deprecated
-    public boolean isReadErr() {
-        return readErr;
-    }
+  @Deprecated
+  public boolean isReadOut() {
+    return readOut;
+  }
 
-    public CmdBuilder addArg(Object arg) {
-        if (arg != null) {
-            if (arg instanceof Collection<?>) {
-                for (Object o : (Collection<?>) arg) {
-                    this.addArg(o);
-                }
-            } else if (arg.getClass().isArray()) {
-                for (int i = 0; i < Array.getLength(arg); i++) {
-                    this.addArg(Array.get(arg, i));
-                }
+  @Deprecated
+  public CmdBuilder setReadErr(boolean readErr) {
+    this.readErr = readErr;
+    return this;
+  }
 
-            } else {
-                cmds.add(arg.toString());
-            }
+  @Deprecated
+  public boolean isReadErr() {
+    return readErr;
+  }
 
+  public CmdBuilder addArg(Object arg) {
+    if (arg != null) {
+      if (arg instanceof Collection<?>) {
+        for (Object o : (Collection<?>) arg) {
+          this.addArg(o);
         }
-        return this;
-    }
-
-    public CmdBuilder addArgs(Object... args) {
-        if (args != null) {
-            for (Object arg : args) {
-                this.addArg(arg);
-            }
+      } else if (arg.getClass().isArray()) {
+        for (int i = 0; i < Array.getLength(arg); i++) {
+          this.addArg(Array.get(arg, i));
         }
-        return this;
+
+      } else {
+        cmds.add(arg.toString());
+      }
+
     }
+    return this;
+  }
 
-    public String[] getCmdArray() {
-        return cmds.toArray(new String[]{});
+  public CmdBuilder addArgs(Object... args) {
+    if (args != null) {
+      for (Object arg : args) {
+        this.addArg(arg);
+      }
     }
+    return this;
+  }
 
-    @Override
-    public String toString() {
-        return toString(this.getCmdArray());
-    }
+  public String[] getCmdArray() {
+    return cmds.toArray(new String[]{});
+  }
 
-    public static String toString(String[] cmds) {
-        return ProcessTool.createCommandLine(cmds);
-    }
+  @Override
+  public String toString() {
+    return toString(this.getCmdArray());
+  }
 
-    public CmdResult exec() {
-        return exec(null);
-    }
+  public static String toString(String[] cmds) {
+    return ProcessTool.createCommandLine(cmds);
+  }
 
-    public CmdResult exec(PreExecute preExecute) {
-        return OSProxyFactory.factory.getOSProxy().exec(this, preExecute);
-    }
+  @Override
+  public CmdResult call() throws Exception {
+    return exec();
+  }
 
-    public ProcessBuilder processBuilder() throws IOException {
-        return OSProxyFactory.factory.getOSProxy().processBuilder(this);
-    }
+  public CmdResult exec() {
+    return OSProxyFactory.factory.getOSProxy().exec(this, preExecute);
+  }
 
-    public static CmdResult kill(String pid) {
-        return OSProxyFactory.factory.getOSProxy().kill(pid);
-    }
+  public CmdResult exec(PreExecute preExecute) {
+    return this.setPreExecute(preExecute).exec();
+  }
 
-    public static CmdBuilder create(String cmd) {
-        return new CmdBuilder(cmd);
-    }
+  public ProcessBuilder processBuilder() throws IOException {
+    return OSProxyFactory.factory.getOSProxy().processBuilder(this);
+  }
 
-    public static CmdBuilder create(String cmd, Object... args) {
-        CmdBuilder builder = new CmdBuilder(cmd);
-        builder.addArgs(args);
-        return builder;
-    }
+  public static CmdResult kill(String pid) {
+    return OSProxyFactory.factory.getOSProxy().kill(pid);
+  }
 
+  public static CmdBuilder create(String cmd) {
+    return new CmdBuilder(cmd);
+  }
 
-    public static void main(String[] args) {
-        CmdBuilder b = CmdBuilder.create("test", "d1", "d2", new int[]{3, 4, 5}, Arrays.asList("a6", 7, "a8"), "d9");
+  public static CmdBuilder create(String cmd, Object... args) {
+    CmdBuilder builder = new CmdBuilder(cmd);
+    builder.addArgs(args);
+    return builder;
+  }
 
-        System.out.println(b.cmds);
-    }
+  public static void main(String[] args) {
+    CmdBuilder b = CmdBuilder.create("test", "d1", "d2", new int[]{3, 4, 5}, Arrays.asList("a6", 7, "a8"), "d9");
+
+    System.out.println(b.cmds);
+  }
 
 }
--- src/main/java/com/ls/luava/os/PreExecute.java
+++ src/main/java/com/ls/luava/os/PreExecute.java
@@ -1,5 +1,6 @@
 package com.ls.luava.os;
 
+@FunctionalInterface
 public interface PreExecute {
   void preExecute(ProcessBuilder processBuilder);
 }
